b99f3bde3a13f624e11071fed2585e05bd48964d
jinze <46251666+userljz@users.noreply.github.com>
2025-05-13 16:45:10 +0800

    Update cnets.py

--- gumiho/model/cnets.py
+++ gumiho/model/cnets.py
@@ -17,6 +17,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Modifications Copyright © 2025 Advanced Micro Devices, Inc. All rights reserved.
+
 """ PyTorch LLaMA model."""
 import copy
 import os
