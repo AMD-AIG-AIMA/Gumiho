1993be87503d64d64b6766f8bdba2e3fb8512770
jinze <46251666+userljz@users.noreply.github.com>
2025-05-13 15:28:23 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,7 +5,7 @@ logger_name="logger_name_to_be_set"
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed main_deepspeed.py --deepspeed --deepspeed_config ds_config.json \
     --tmpdir "pre_generated_data" \
     --cpdir "path_to_save_ckpt" \
-    --configpath "../gumiho/train/EAGLE-LLaMA3-Instruct-70B.json" \
+    --configpath "../gumiho/train/Gumiho-LLaMA3-Instruct-70B.json" \
     --basepath "target_model_path" \
     --logger_file ${logger_name} \
     --p_w 0.1 \
